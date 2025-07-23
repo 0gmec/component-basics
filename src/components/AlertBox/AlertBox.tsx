c924778b7a286e180ae7a90b2bb8acf41fc5fe56
mlsfiles75@gmail.com <mlsfiles75@gmail.com>
2025-07-23 18:25:31 -0400

    added react

---
+++ src/components/AlertBox/AlertBox.tsx
@@ -0,0 +1,12 @@
+//  export type AlertType = 'success' | 'error' | 'warning' | 'info';
+ 
+// export interface AlertBoxProps {
+//   type: AlertType;
+//   message: string;
+//   onClose?: () => void;
+//   children?: React.ReactNode;
+// }
+
+export default function AlertBox() {
+    return <p>hello</p>
+}
